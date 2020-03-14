736ae0556dedfb1df08e45108758d852898d0b81
ssp <ssp@gmail.com>
2020-03-14 15:56:24 +0900

    lambda 오타 해결

--- lambda/index.js
+++ lambda/index.js
@@ -6,8 +6,8 @@ const S3 = new AWS.S3({ region: 'ap-northeast02'});
 exports.handler = async (event, context, callback) => {
     const Bucket = event.Records[0].s3.bucket.name;
     const Key = event.Records[0].s3.object.key;
-    const filename = Key.split('/')[[key.split('/').length - 1]];
-    const ext = Key.split('.')[[Key.split('.').length -1 ]];
+    const filename = Key.split('/')[key.split('/').length - 1];
+    const ext = Key.split('.')[Key.split('.').length -1 ];
     console.log(Bucket, key, filename, ext );
 
     const requiredFormat = ext === 'jpg' || ext === 'png' || ext === 'jfif' ? 'jpeg' : ext;
