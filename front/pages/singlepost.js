d438609fd838dc2a22bb29e344470710bbe212ed
ssp <ssp@gmail.com>
2020-03-17 15:39:44 +0900

    최종본

--- front/pages/singlepost.js
+++ front/pages/singlepost.js
@@ -7,6 +7,7 @@ import Helmet from 'react-helmet';
 
 const SinglePost = ({singlePostId}) => {
     const { mainPosts } = useSelector(state => state.post); 
+    console.log('SinglePost mainPosts 값 : ', mainPosts);
 
     return (
     <>
