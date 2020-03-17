0fb88963ebcd4c2f4eeb530b269aaf5c635ebdc4
ssp <ssp@gmail.com>
2020-03-17 15:29:29 +0900

    오류 수정

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -266,6 +266,7 @@ export default (state = initialState, action) => {
 
             case LOAD_SINGLE_POST_SUCCESS: {
                 draft.mainPosts = action.data;
+                console.log('LOAD_SINGLE_POST_SUCCESS 값 : ', action.data);
                 break;
             }
 
